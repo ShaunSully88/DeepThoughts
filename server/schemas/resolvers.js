408103a704268946f77f2619ad130059073832d3
Shaun Sullivan <shaunsully88@gmail.com>
2022-07-07 14:05:26 -0400

    everything complete

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,7 +3,6 @@ const { User, Thought } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
-<<<<<<< HEAD
   Query: {
     me: async (parent, args, context) => {
       if (context.user) {
@@ -11,31 +10,6 @@ const resolvers = {
           .select('-__v -password')
           .populate('thoughts')
           .populate('friends');
-=======
-    Query: {
-       thoughts: async (parent, {username}) => {
-        const params = username ? { username } : {};
-        return Thought.find(params).sort({ createdAt: -1 });
-       },
-       thought: async (parent, { _id }) => {
-        return Thought.findOne({ _id });
-       },
-        users: async () => {
-            return User.find()
-            .select('-__v -password')
-            .populate('friends')
-            .populate('thoughts');
-        },
-        user: async (parent, { username }) => {
-            return User.findOne({ username })
-            .select('-__v -password')
-            .populate('friends')
-            .populate('thoughts')
-        }
-        }
-       
-    };
->>>>>>> feature/routes
 
         return userData;
       }
--- server/server.js
+++ server/server.js
@@ -1,16 +1,16 @@
-const path = require('path');
 const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
+const {ApolloServer} = require('apollo-server-express');
 const path = require('path');
 
-const { typeDefs, resolvers } = require('./schemas');
-const { authMiddleware } = require('./utils/auth');
+const {typeDefs, resolvers} = require('./schemas');
+const {authMiddleware} = require('./utils/auth');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware,
 });
 
 const app = express();
@@ -18,10 +18,18 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
-<<<<<<< HEAD
   server.applyMiddleware({ app });
   
   db.once('open', () => {
@@ -34,24 +42,3 @@ const startApolloServer = async (typeDefs, resolvers) => {
   
   // Call the async function to start the server
   startApolloServer(typeDefs, resolvers);
-=======
-  server.applyMiddleware({ app })
-}
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
-
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-  });
-});
-
-startApolloServer(typeDefs, resolvers);
->>>>>>> feature/routes
